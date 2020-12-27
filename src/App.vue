68f435ae861eee86597d7fd8f58ed46400d987b4
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-27 11:36:07 +0600

    Done create componenet & import

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+ 
+    
+    {{name}}
+    <br>
+    {{job}}
+
+    <br>
+
+
+       <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 
 export default {
-  name: 'App',
+  
+  data(){
+    return {
+      name: "Kawsar Ahmed",
+      job: "Learning Web Development"
+    }
+  },
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  
 </style>
