34832a317ef80c4b4d13975f7539a7604da6c992
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-28 13:12:26 +0600

    integrate bootstrap

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,26 @@
       <p :slot="loader">Paragraph</p>
     </compHelper>
 
+  <BootstrapFrom></BootstrapFrom>
+
   </div>
 </template>
 
 <script>
 import compHelper from "./Components/ListHelper/ListHelper";
+import BootstrapFrom from "./Components/BootstrapForm/BootstrapForm";
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 
 export default {
   
@@ -26,7 +41,8 @@ export default {
     }
   },
   components: {
-    compHelper
+    compHelper,
+    BootstrapFrom
   },
   created(){
     setTimeout(() => {
